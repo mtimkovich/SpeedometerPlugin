edc0e75e5bca814c172a67b676ab772d2f2e9472
Max Timkovich <mtimkovich@gmail.com>
2021-04-17 14:13:24 -0600

    remove tabs

--- Speedometer/Speedometer.cpp
+++ Speedometer/Speedometer.cpp
@@ -8,13 +8,13 @@ BAKKESMOD_PLUGIN(Speedometer, "Speedometer", "0.1", PLUGINTYPE_FREEPLAY)
 
 void Speedometer::onLoad()
 {
-	useMetric = make_shared<bool>(false);
+    useMetric = make_shared<bool>(false);
 
     cvarManager->registerCvar("Speedometer_Metric", "0", "Show speed in KPH or MPH")
         .bindTo(useMetric);
 
-	xPos = make_shared<int>(0);
-	yPos = make_shared<int>(0);
+    xPos = make_shared<int>(0);
+    yPos = make_shared<int>(0);
     cvarManager->registerCvar("Speedometer_X_Position", "25", "X position",
             true, true, 0, true, 2560).bindTo(xPos);
     cvarManager->registerCvar("Speedometer_Y_Position", "245", "Y position",
@@ -27,7 +27,7 @@ void Speedometer::onLoad()
             "Function GameEvent_Soccar_TA.ReplayPlayback.EndState",
             [&](std::string eventName){isInGoalReplay = false; });
 
-	gameWrapper->RegisterDrawable(bind(&Speedometer::Render, this, std::placeholders::_1));
+    gameWrapper->RegisterDrawable(bind(&Speedometer::Render, this, std::placeholders::_1));
 }
 
 void Speedometer::onUnload(){}
@@ -49,25 +49,25 @@ void Speedometer::drawText(CanvasWrapper canvas, string text) {
 
 // Get the car's speed, or -1 if we don't want to render.
 float Speedometer::getCarSpeed() {
-	CameraWrapper camera = gameWrapper->GetCamera();
+    CameraWrapper camera = gameWrapper->GetCamera();
 
-	if (isInGoalReplay || camera.IsNull()) {
-		return -1;
-	}
+    if (isInGoalReplay || camera.IsNull()) {
+        return -1;
+    }
 
-	PriWrapper target = PriWrapper(
+    PriWrapper target = PriWrapper(
             reinterpret_cast<std::uintptr_t>(
                 gameWrapper->GetCamera().GetViewTarget().PRI));
-	if (target.IsNull()) {
-		return -1;
-	}
+    if (target.IsNull()) {
+        return -1;
+    }
 
-	CarWrapper car = target.GetCar();
+    CarWrapper car = target.GetCar();
     if (car.IsNull()) {
         return -1;
     }
 
-	return car.GetVelocity().magnitude();
+    return car.GetVelocity().magnitude();
 }
 
 bool validGameState(shared_ptr<GameWrapper> gameWrapper) {
--- Speedometer/Speedometer.h
+++ Speedometer/Speedometer.h
@@ -5,17 +5,16 @@
 class Speedometer : public BakkesMod::Plugin::BakkesModPlugin
 {
 private:
-	bool isInGoalReplay = false;
-	std::shared_ptr<bool> useMetric;
-	std::shared_ptr<int> xPos;
-	std::shared_ptr<int> yPos;
+    bool isInGoalReplay = false;
+    std::shared_ptr<bool> useMetric;
+    std::shared_ptr<int> xPos;
+    std::shared_ptr<int> yPos;
 
-public:
-	void onLoad() override;
-	void onUnload() override;
-
-    Vector2 textPosition(CanvasWrapper canvas);
     float Speedometer::getCarSpeed();
     void drawText(CanvasWrapper canvas, std::string text);
-	void Render(CanvasWrapper canvas);
+    void Render(CanvasWrapper canvas);
+
+public:
+    void onLoad() override;
+    void onUnload() override;
 };
